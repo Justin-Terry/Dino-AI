692323daabcb8e621d310bca7eb6d3392b50ddcb
Justin Terry <Justin.Terry@student.csulb.edu>
2019-10-10 17:30:41 -0700

    Initial commit

---
+++ Screen_Grab.py
@@ -0,0 +1,32 @@
+import time
+
+import cv2
+import mss
+import keyboard
+import numpy
+
+
+with mss.mss() as sct:
+    # Part of the screen to capture
+    monitor = {"top": 200, "left": 170, "width": 1, "height": 56}
+
+    while "Screen capturing":
+        #last_time = time.time()
+
+        # Get raw pixels from the screen, save it to a Numpy array
+        img = numpy.array(sct.grab(monitor))
+
+        if(img[45][0][1] < 255 or img[20][0][1] < 255):
+            keyboard.press('space')
+        # Display the picture
+        #cv2.imshow("OpenCV/Numpy normal", img)
+
+        # Display the picture in grayscale
+        # cv2.imshow('OpenCV/Numpy grayscale',cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+
+        # print("fps: {}".format(1 / (time.time() - last_time)))
+
+        # Press "q" to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
